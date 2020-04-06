ac339e1bf5b2baeb5557f7b7206463d36a5290e0
PetarTolev <petar.dtolev@gmail.com>
2020-04-07 00:23:20 +0300

    Refactor startup.

---
+++ Web/LearnLanguageSystem.Web/Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,90 @@
+﻿namespace LearnLanguageSystem.Web.Infrastructure
+{
+    using LearnLanguageSystem.Data;
+    using LearnLanguageSystem.Data.Common;
+    using LearnLanguageSystem.Data.Common.Repositories;
+    using LearnLanguageSystem.Data.Models;
+    using LearnLanguageSystem.Data.Repositories;
+    using LearnLanguageSystem.Services.Data;
+    using LearnLanguageSystem.Services.Data.Answers;
+    using LearnLanguageSystem.Services.Data.Contests;
+    using LearnLanguageSystem.Services.Data.Questions;
+    using LearnLanguageSystem.Services.Messaging;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+            => services
+                .AddDbContext<ApplicationDbContext>(
+                    options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+            => services
+                .AddScoped(typeof(IDeletableEntityRepository<>), typeof(EfDeletableEntityRepository<>))
+                .AddScoped(typeof(IRepository<>), typeof(EfRepository<>))
+                .AddScoped<IDbQueryRunner, DbQueryRunner>()
+                .AddTransient<IEmailSender, NullMessageSender>()
+                .AddTransient<ISettingsService, SettingsService>()
+                .AddTransient<IContestsService, ContestsService>()
+                .AddTransient<IQuestionsService, QuestionsService>()
+                .AddTransient<IAnswersService, AnswersService>();
+
+        public static IServiceCollection AddApplicationConfigurations(this IServiceCollection services)
+        {
+            services
+                .AddControllersWithViews()
+                .AddRazorRuntimeCompilation();
+
+            services.AddDefaultIdentity<ApplicationUser>(IdentityOptionsProvider.GetIdentityOptions)
+                .AddRoles<ApplicationRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.Configure<CookiePolicyOptions>(
+                options =>
+                {
+                    options.CheckConsentNeeded = context => true;
+                    options.MinimumSameSitePolicy = SameSiteMode.None;
+                });
+
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+
+                options.User.RequireUniqueEmail = false;
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddExternalLogins(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication()
+                .AddFacebook(options =>
+                {
+                    options.AppId = configuration["FacebookSettings:AppId"];
+                    options.AppSecret = configuration["FacebookSettings:AppSecret"];
+                })
+                .AddGoogle(options =>
+                {
+                    options.ClientId = configuration["GoogleSettings:ClientId"];
+                    options.ClientSecret = configuration["GoogleSettings:ClientSecret"];
+                });
+
+            return services;
+        }
+
+    }
+}
--- Web/LearnLanguageSystem.Web/Startup.cs
+++ Web/LearnLanguageSystem.Web/Startup.cs
@@ -3,22 +3,12 @@
     using System.Reflection;
 
     using LearnLanguageSystem.Data;
-    using LearnLanguageSystem.Data.Common;
-    using LearnLanguageSystem.Data.Common.Repositories;
-    using LearnLanguageSystem.Data.Models;
-    using LearnLanguageSystem.Data.Repositories;
     using LearnLanguageSystem.Data.Seeding;
-    using LearnLanguageSystem.Services.Data;
-    using LearnLanguageSystem.Services.Data.Answers;
-    using LearnLanguageSystem.Services.Data.Contests;
-    using LearnLanguageSystem.Services.Data.Questions;
     using LearnLanguageSystem.Services.Mapping;
-    using LearnLanguageSystem.Services.Messaging;
+    using LearnLanguageSystem.Web.Infrastructure;
     using LearnLanguageSystem.Web.ViewModels;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.Http;
-    using Microsoft.AspNetCore.Identity;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
@@ -28,79 +18,20 @@
     {
         private readonly IConfiguration configuration;
 
-        public Startup(IConfiguration configuration)
-        {
-            this.configuration = configuration;
-        }
+        public Startup(IConfiguration configuration) => this.configuration = configuration;
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+        => services
+                .AddDatabase(this.configuration)
+                .AddApplicationConfigurations()
+                .AddExternalLogins(this.configuration)
+                .AddSingleton(this.configuration)
+                .AddApplicationServices();
 
-            services.AddDbContext<ApplicationDbContext>(
-                options => options.UseSqlServer(this.configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddDefaultIdentity<ApplicationUser>(IdentityOptionsProvider.GetIdentityOptions)
-                .AddRoles<ApplicationRole>().AddEntityFrameworkStores<ApplicationDbContext>();
-
-            services.Configure<CookiePolicyOptions>(
-                options =>
-                    {
-                        options.CheckConsentNeeded = context => true;
-                        options.MinimumSameSitePolicy = SameSiteMode.None;
-                    });
-
-            services.AddControllersWithViews();
-            services.AddRazorPages();
-
-            services.Configure<IdentityOptions>(options =>
-            {
-                // Password settings.
-                options.Password.RequireDigit = true;
-                options.Password.RequireLowercase = false;
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireUppercase = false;
-                options.Password.RequiredLength = 6;
-                options.Password.RequiredUniqueChars = 1;
-
-                options.User.RequireUniqueEmail = false;
-            });
-
-            services.AddAuthentication()
-                .AddFacebook(options =>
-                {
-                    options.AppId = this.configuration["FacebookSettings:AppId"];
-                    options.AppSecret = this.configuration["FacebookSettings:AppSecret"];
-                })
-                .AddGoogle(options =>
-                {
-                    options.ClientId = this.configuration["GoogleSettings:ClientId"];
-                    options.ClientSecret = this.configuration["GoogleSettings:ClientSecret"];
-                });
-
-            services.AddSingleton(this.configuration);
-
-            // Data repositories
-            services.AddScoped(typeof(IDeletableEntityRepository<>), typeof(EfDeletableEntityRepository<>));
-            services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
-            services.AddScoped<IDbQueryRunner, DbQueryRunner>();
-
-            // Application services
-            services.AddTransient<IEmailSender, NullMessageSender>();
-
-            services.AddTransient<ISettingsService, SettingsService>();
-            services.AddTransient<IContestsService, ContestsService>();
-            services.AddTransient<IQuestionsService, QuestionsService>();
-            services.AddTransient<IAnswersService, AnswersService>();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             AutoMapperConfig.RegisterMappings(typeof(ErrorViewModel).GetTypeInfo().Assembly);
 
-            // Seed data on application startup
             using (var serviceScope = app.ApplicationServices.CreateScope())
             {
                 var dbContext = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
@@ -110,7 +41,8 @@
                     dbContext.Database.Migrate();
                 }
 
-                new ApplicationDbContextSeeder().SeedAsync(dbContext, serviceScope.ServiceProvider).GetAwaiter().GetResult();
+                new ApplicationDbContextSeeder().SeedAsync(dbContext, serviceScope.ServiceProvider).GetAwaiter()
+                    .GetResult();
             }
 
             if (env.IsDevelopment())
@@ -124,19 +56,17 @@
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
-
-            app.UseRouting();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.UseEndpoints(
-                endpoints =>
-                {
-                        endpoints.MapControllerRoute("areaRoute", "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+            app.UseHttpsRedirection()
+                .UseStaticFiles()
+                .UseCookiePolicy()
+                .UseRouting()
+                .UseAuthentication()
+                .UseAuthorization()
+                .UseEndpoints(
+                    endpoints =>
+                    {
+                        endpoints.MapControllerRoute("areaRoute",
+                            "{area:exists}/{controller=Home}/{action=Index}/{id?}");
                         endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
                         endpoints.MapRazorPages();
                     });
