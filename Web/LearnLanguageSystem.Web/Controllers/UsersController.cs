185f5d3a915001ce4890150666c73a54ae66edde
PetarTolev <petar.dtolev@gmail.com>
2020-04-17 22:47:00 +0300

    Rearranging user profile and add avatar.

---
+++ Web/LearnLanguageSystem.Web/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using LearnLanguageSystem.Services.Data.Cloudinary;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearnLanguageSystem.Web.Controllers
+{
+    public class UsersController : BaseController
+    {
+        public UsersController()
+        {
+        }
+
+        public IActionResult ChangeAvatar()
+        {
+            return this.NoContent();
+        }
+    }
+}
