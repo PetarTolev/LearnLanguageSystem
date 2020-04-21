11a16cc2a404c4c5e9c68df59ab916daaeb26995
PetarTolev <petar.dtolev@gmail.com>
2020-04-21 23:50:31 +0300

    Add ranking controller, little changes in layout.

---
+++ Web/LearnLanguageSystem.Web/Controllers/RankingController.cs
@@ -0,0 +1,12 @@
+﻿namespace LearnLanguageSystem.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class RankingController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return this.NoContent();
+        }
+    }
+}
