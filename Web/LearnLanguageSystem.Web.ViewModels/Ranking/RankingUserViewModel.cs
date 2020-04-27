1adfe0fe48d6960c5acba70fc48b85356245270f
PetarTolev <petar.dtolev@gmail.com>
2020-04-27 16:45:26 +0300

    Implement ranking.

---
+++ Web/LearnLanguageSystem.Web.ViewModels/Ranking/RankingUserViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace LearnLanguageSystem.Web.ViewModels.Ranking
+{
+    using LearnLanguageSystem.Data.Models;
+    using LearnLanguageSystem.Services.Mapping;
+
+    public class RankingUserViewModel : IMapFrom<ApplicationUser>
+    {
+        public string UserName { get; set; }
+
+        public string AvatarUrl { get; set; }
+
+        public int PointsFromContests { get; set; }
+    }
+}
--- Web/LearnLanguageSystem.Web/Controllers/RankingController.cs
+++ Web/LearnLanguageSystem.Web/Controllers/RankingController.cs
@@ -1,12 +1,35 @@
 ﻿namespace LearnLanguageSystem.Web.Controllers
 {
+    using System.Linq;
+
+    using LearnLanguageSystem.Data;
+    using LearnLanguageSystem.Web.ViewModels.Ranking;
     using Microsoft.AspNetCore.Mvc;
 
     public class RankingController : BaseController
     {
+        private readonly ApplicationDbContext context;
+
+        public RankingController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
         public IActionResult Index()
         {
-            return this.NoContent();
+            var model = this.context.Users
+                .Select(x =>
+                    new RankingUserViewModel
+                    {
+                        UserName = x.UserName,
+                        AvatarUrl = x.AvatarUrl,
+                        PointsFromContests = x.PointsFromContests,
+                    })
+                .OrderByDescending(x => x.PointsFromContests)
+                .ThenBy(x => x.UserName)
+                .ToList();
+
+            return this.View(model);
         }
     }
 }
