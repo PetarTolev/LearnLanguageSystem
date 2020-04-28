baa1f6be656c053e0a258aeee3f90b23345d4b24
PetarTolev <petar.dtolev@gmail.com>
2020-04-28 21:42:53 +0300

    Add JoinedRoom ViewComponent.

---
+++ Web/LearnLanguageSystem.Web/Components/JoinedRoomComponent.cs
@@ -0,0 +1,29 @@
+﻿namespace LearnLanguageSystem.Web.Components
+{
+    using LearnLanguageSystem.Data.Models;
+    using LearnLanguageSystem.Services.Data.Rooms;
+    using LearnLanguageSystem.Web.ViewModels.Rooms;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class JoinedRoomComponent : ViewComponent
+    {
+        private readonly IRoomsService roomsService;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public JoinedRoomComponent(IRoomsService roomsService, UserManager<ApplicationUser> userManager)
+        {
+            this.roomsService = roomsService;
+            this.userManager = userManager;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var user = this.userManager.GetUserAsync(this.HttpContext.User).GetAwaiter().GetResult();
+
+            var model = this.roomsService.GetById<JoinedRoomViewModel>(user.RoomId);
+
+            return this.View(model);
+        }
+    }
+}
